ee34ad055838cf6bad04e0e3da573c0e0377acfd
davidkiama <davidkiama98@gmail.com>
2022-03-26 10:23:46 +0300

    Styled the image description

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -3,11 +3,23 @@ const galleryUl = document.querySelector(".gallery").querySelector("ul");
 //Loop and in each iteration render an image randomly
 // step maxValue is the numbers of images to render
 for (let step = 0; step < 15; step++) {
-  let imgInd = Math.floor(Math.random() * 9) + 1;
+  let imgInd = Math.floor(Math.random() * 12) + 1;
 
   const imageHtml = `
-    <li>
-        <img src="static/img/gal-${imgInd}.jpg" alt="" loading="lazy" class="gal-img" />
-    </li>`;
+  <li class="gal-list">
+  <img src="static/img/gal-${imgInd}.jpg" alt="" loading="lazy"
+      class="gal-img" />
+    <div class="gal-list-desc">
+      <div>
+          <p class="gal-list__category">Category</p>
+          <p class="gal-list__location">Location</p>
+      </div>
+
+        <img src="static/img/copy.svg" class="gal-list__copy" />
+    </div>
+    </li>
+  
+    
+  `;
   galleryUl.insertAdjacentHTML("beforeend", imageHtml);
 }
