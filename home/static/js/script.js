5d695595d1507a976d161b88e0758c478fcac779
davidkiama <davidkiama98@gmail.com>
2022-03-26 20:54:35 +0300

    Display images from the database

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -2,40 +2,39 @@ const galleryUl = document.querySelector(".gallery").querySelector("ul");
 
 //Loop and in each iteration render an image randomly
 // step maxValue is the numbers of images to render
-for (let step = 0; step < 15; step++) {
-  let imgInd = Math.floor(Math.random() * 12) + 1;
+// for (let step = 0; step < 15; step++) {
+//   let imgInd = Math.floor(Math.random() * 12) + 1;
 
-  const imageHtml = `
-  <li class="gal-list">
-  <img src="static/img/gal-${imgInd}.jpg" alt="" loading="lazy"
-      class="gal-img" />
-    <div class="gal-list-desc">
-      <div>
+//   const imageHtml = `
+//   <li class="gal-list">
+//   <img src="static/img/gal-${imgInd}.jpg" alt="" loading="lazy"
+//       class="gal-img" />
+//     <div class="gal-list-desc">
+//       <div>
 
-          <p class="gal-list__category">
-          
-          <svg class="icon">
-            <use xlink:href="static/img/sprite.svg#hashtag"></use>
-          </svg>
-          
-            Category
-          </p>
-          <p class="gal-list__location">
+//           <p class="gal-list__category">
 
-          <svg class="icon">
-            <use xlink:href="static/img/sprite.svg#location"></use>
-          </svg>
-            Location
-          </p>
-      </div>
+//           <svg class="icon">
+//             <use xlink:href="static/img/sprite.svg#hashtag"></use>
+//           </svg>
 
-      <svg class="icon">
-      <use xlink:href="static/img/sprite.svg#copy"></use>
-    </svg>
-    </div>
-    </li>
-  
-    
-  `;
-  galleryUl.insertAdjacentHTML("beforeend", imageHtml);
-}
+//             Category
+//           </p>
+//           <p class="gal-list__location">
+
+//           <svg class="icon">
+//             <use xlink:href="static/img/sprite.svg#location"></use>
+//           </svg>
+//             Location
+//           </p>
+//       </div>
+
+//       <svg class="icon">
+//       <use xlink:href="static/img/sprite.svg#copy"></use>
+//     </svg>
+//     </div>
+//     </li>
+
+//   `;
+//   galleryUl.insertAdjacentHTML("beforeend", imageHtml);
+// }
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Image, Location, Category
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    images = Image.get_all_images()
+    return render(request, 'index.html', {'images': images})
